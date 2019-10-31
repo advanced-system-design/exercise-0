1dae5a78106f91eef3a3c1a0c7326d572c4c2d9e
Dan Gittik <dgittik@magicleap.com>
2019-10-31 14:46:46 +0200

    Fixed installation script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,8 +7,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 function main {
     python -m virtualenv .env --prompt "[exercise 0] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
-    pip install -U pip
-    pip install -r requirements.txt
+    .env/bin/pip install -U pip
+    .env/bin/pip install -r requirements.txt
 }
 
 
